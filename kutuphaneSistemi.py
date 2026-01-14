25e374940774e00b3aa6d20d97256627a0edee44
Cem Gökmen <cemgokmen101@gmail.com>
2026-01-14 10:47:23 +0300

    V1: Temel sistem(ekleme , listeleme , odunc)

---
+++ kutuphaneSistemi.py
@@ -0,0 +1,53 @@
+class Kutuphane:
+    def __init__(self):
+        self.kitaplar = {
+        "Sefiller": {"yazar":"Victor Hugo","durum":"Rafta"},
+        "Nutuk": {"yazar":"Mustafa Kemal Atatürk","durum":"Rafta"},
+        "1984": {"yazar":"George Orwell","durum":"Rafta"}
+    }
+    
+    def kitaplari_listele(self):
+            print("---KÜTÜPHANE ARŞİVİ---")
+            for ad, bilgi in self.kitaplar.items():
+                durum_ikonu = "✅" if bilgi["durum"] == "Rafta" else "❌"
+                print(f"{durum_ikonu}{ad}-yazar: {bilgi['yazar']}({bilgi['durum']})")
+                
+    def kitap_ekle(self):
+        kitap_ad = input("Eklenecek kitap adını giriniz: ")
+        yazar_ad = input("Eklenecek kitabın yazarını giriniz: ")
+        self.kitaplar[kitap_ad] = {"yazar": yazar_ad , "durum": "Rafta"}
+        print(f">>> '{kitap_ad}' Başarıyla sisteme eklendi ✅\n")
+    
+    def kitap_odunc_al(self):
+        self.kitaplari_listele()
+        secim = input("Ödünç almak istediğiniz kitabın adını giriniz: ")
+        if secim in self.kitaplar:
+            if self.kitaplar[secim]["durum"] == "Rafta":
+                self.kitaplar[secim]["durum"] == "Ödünç verildi"
+                print(f"Başarılı! {secim} kitabını aldınız")
+            else:
+                print("Kitap şu an Başkasında ")
+        else:
+            print("Sistemimizde böyle bir kitap bulunmamaktadır")
+            
+
+kutuphane = Kutuphane()
+while True:
+    print("1. Listele\n2. Ekle\n3. Ödünç Al\n4. Çıkış\n")
+    secim = input("Lütfen yapmak istediğiniz işlemi tuşlayınız: ")
+    if secim == '1':
+        kutuphane.kitaplari_listele()
+    elif secim == '2':
+        kutuphane.kitap_ekle()
+    elif secim == '3':
+        kutuphane.kitap_odunc_al()
+    elif secim == '4':
+        break
+    else:
+        print("Hatalı değer tuşladınız...")
+        
+        
+        
+        
+    
+    
